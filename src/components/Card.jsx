576478f4f521abbf46c6ffb6c909da0966d2cdeb
Jorge <jorgeluizkoch@gmail.com>
2025-03-13 14:13:51 -0300

    new styled-components add

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import seta from '../images/seta_play.png';
 
 export default function Card({ question, answer, i }) {
+
     const [isBlank, setIsBlank] = useState(true);
     const [questionCard, setQuestionCard] = useState(false);
     const [answerCard, setAnswerCard] = useState(false);
@@ -18,26 +19,51 @@ export default function Card({ question, answer, i }) {
     }
 
     return (
-        <CardStyle onClick={cardClick}>
-            {isBlank && <div>Pergunta <img src={seta} alt="seta"></img></div>}
-            {questionCard && <div>{question}</div>}
-            {answerCard && <div>{answer}</div>}
-        </CardStyle>
+        <BlankStyle onClick={cardClick}>
+            <>{isBlank && <div> Pergunta <img src={seta} alt="seta"></img></div>}</>
+            <>{questionCard && <div>{question}</div>}</>
+            <>{answerCard && <div>{answer}</div>}</>
+        </BlankStyle>
     );
 }
 
-const CardStyle = styled.div`
+const BlankStyle = styled.div`
     display: flex;
     flex-direction: row;
     align-items: center;
     justify-content: space-evenly;
     width: 300px;
     height: 65px;
+    margin: 20px;
     background-color: #fff;
     border-radius: 5px;
     box-shadow: 0px 4px 5px 0px #00000026;
-    margin: 10px;
-    padding: 20px;
     font-size: 16px;
     color: #333333;
+`
+
+const QuestionStyle = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    width: 300px;
+    height: 131px;
+    margin: 20px;
+    background-color: #FFFFD4;
+    border-radius: 5px;
+    box-shadow: 0px 4px 5px 0px #00000026;
+`
+
+const AnswerStyle = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    width: 300px;
+    height: 131px;
+    margin: 20px;
+    background-color: #FFFFD4;
+    border-radius: 5px;
+    box-shadow: 0px 4px 5px 0px #00000026;
 `
